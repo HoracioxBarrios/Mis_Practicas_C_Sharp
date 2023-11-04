7f264dc5e2b93b00b4a32bc82e3e2122d038fa1d
horacioxbarrios <horacioxbarrios@gmail.com>
2023-11-03 23:11:48 -0300

    tipo de dato nuevo

---
+++ practica_ tipo de dato nuevo/Aplicacion/Program.cs
@@ -0,0 +1,27 @@
+﻿using Biblioteca;
+
+namespace Aplicacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Direccion direccion = new Direccion
+            {
+
+                Ciudad = "BsAs",
+                Calle = "San Martin",
+                Estado = "Calzada",
+                CodigoPostal = "1847"
+
+            };
+
+
+            //Console.WriteLine(direccion.GetType());
+
+            Persona persona = new Persona("Sio", direccion);
+            Console.WriteLine(persona.ToString());
+
+        }
+    }
+}
---
+++ practica_ tipo de dato nuevo/Biblioteca/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Persona
+    {
+        private Direccion _direccion;
+        private string _nombre;
+
+        public Persona(string nombre, Direccion direccion)
+        {
+            _nombre = nombre;
+            _direccion = direccion;
+        }
+
+        public Direccion Direccion
+        {
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+
+
+        //sobrecarga del metodo de la clase cosmica
+        public override string ToString()
+        {
+            // Define cómo quieres que se vea la representación de la persona como cadena
+            return $"Nombre: {_nombre}, Dirección: {_direccion} ";
+        }
+    }
+}
