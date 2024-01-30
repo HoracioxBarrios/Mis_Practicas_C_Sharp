5f160342425239a28db953a420180291602411d7
horacio barrios <horacioxbarrios@gmail.com>
2024-01-29 23:09:36 -0300

    Estructuras

--- 1_practicas de curso de mouredev/AplicacionDeConsola/AplicacionDeConsola/Program.cs
+++ 1_practicas de curso de mouredev/AplicacionDeConsola/AplicacionDeConsola/Program.cs
@@ -7,7 +7,7 @@ namespace AplicacionDeConsola // espaco de nombre que creamos (el nuestro)
         static void Main(string[] args)
         {
             Console.WriteLine("Hola C# 2024"); //si no incluyesemos el 'using System' tendrimos que escribir: System.Console.WriteLine()
-            // para comentar en una linea se usa doble barra así.
+                                               // para comentar en una linea se usa doble barra así.
 
             /*para
              comentar
@@ -19,8 +19,12 @@ namespace AplicacionDeConsola // espaco de nombre que creamos (el nuestro)
             el compilador como codigo, sino que lo ignora
             */
 
+            //---------------------------- TIPOS DE DATOS ----------------------------
+            //---------------------------- TIPOS DE DATOS ---------------------------- 
+            //---------------------------- TIPOS DE DATOS ---------------------------- 
 
-            // string es un tipo de datos de cadena de texto o cadena de caracteres
+
+            // ----- TIPO DE DATO CADENA DE CARACTERES string -----
             string myString = "Esto es una cadena de texto";
             Console.WriteLine(myString);
             myString = "Cambio el texto en esta linea";
@@ -28,7 +32,7 @@ namespace AplicacionDeConsola // espaco de nombre que creamos (el nuestro)
 
 
 
-            //int es un tipo de datos de numeros enteros
+            //----- TIPO DE DATO PARA NUMEROS ENTEROS int -----
             int myInt = 7;
             myInt = myInt + 4;
             Console.WriteLine(myInt);// aca vale 11
@@ -36,21 +40,21 @@ namespace AplicacionDeConsola // espaco de nombre que creamos (el nuestro)
             Console.WriteLine(myInt);// aca vale 11 
 
 
-            //tipo de dato para numeros flotantes
-
+            //----- TIPOS DE DATOS PARA NUMEROS FLOTANTES -----
+             
             float myFloat = 1.5F;// se usa sufijo F al final para representar un float [longitud menor]
             Console.WriteLine(myFloat);
 
-            double mydouble = 1.5; // [tiene una longitud mayor] es recomandable usar este en su lugar
-            Console.WriteLine(mydouble);
+            double myDouble = 1.5; // [tiene una longitud mayor] es recomandable usar este en su lugar
+            Console.WriteLine(myDouble);
 
 
-            // #Operaciones
+            // #OPERACIONES:
 
-            Console.WriteLine(mydouble + myFloat + myInt);// se puede operar porque c# trata a todos como datos numericos
+            Console.WriteLine(myDouble + myFloat + myInt);// se puede operar porque c# trata a todos como datos numericos
 
 
-            //tipo de dato dynamic
+            //----- TIPO DE DATO dynamic -----
 
             dynamic myDynamic = "Hola";
             Console.WriteLine(myDynamic);// "hola"
@@ -61,9 +65,9 @@ namespace AplicacionDeConsola // espaco de nombre que creamos (el nuestro)
             dato tiene dentro, para hacer dicha operacion*/
 
 
-            // Palabra reservada var
-            var myvar1 = "es una nueva cadena de texto";
-            Console.WriteLine(myvar1);
+            // ----- PALABRA RESERVADA var -----
+            var myVar1 = "es una nueva cadena de texto";
+            Console.WriteLine(myVar1);
             /*
              * var infiere el tipo de dato en este caso que es string
              
@@ -80,18 +84,97 @@ namespace AplicacionDeConsola // espaco de nombre que creamos (el nuestro)
             Error	CS0029	No se puede convertir implícitamente el tipo 
             'int' en 'string'	AplicacionDeConsola	C:\Users\xxxxxx\Desktop\Practicas_C_Sharp\1_practicas de curso de mouredev\AplicacionDeConsola\AplicacionDeConsola\Program.cs	75	Activo
 */
-            var myvar2 = 100;
-            Console.WriteLine(myvar2.GetType()); //System.Int32 ----- aca usamos GetType() que es un metodo que viene de la clase base  System.Object la cual heredan todos los tipos de referencia (clases) y en cuanto a los tipos de valor struct no heredan directamente de object sino que se implementa la interfaz System.ValueType la cual a su vez deriva de System.Object .
+            var myVar2 = 100;
+            Console.WriteLine(myVar2.GetType()); //System.Int32 ----- aca usamos GetType() que es un metodo que viene de la clase base  System.Object la cual heredan todos los tipos de referencia (clases) y en cuanto a los tipos de valor struct no heredan directamente de object sino que se implementa la interfaz System.ValueType la cual a su vez deriva de System.Object .
 
-            Console.WriteLine(myvar1.GetType()); //System.String
+            Console.WriteLine(myVar1.GetType()); //System.String
 
             //myvar1 = 1.5; //da [error]  porque no se puede cambiar el tipo de dato cuando ya se ha inferido arriba en tiempo de ejecucion.
             
 
-            // Tipo de dato Bool
-            bool mybool = false;
+            // ----- Tipo de dato Bool -----
+            bool myBool = false;
 
-            mybool = true;
+            myBool = true;
+
+            //---------------------------- ******* ---------------------------- 
+            //---------------------------- ******* ---------------------------- 
+            //---------------------------- ******* ---------------------------- 
+
+            // ----- INTERPOLACION DE STRING -----
+            Console.WriteLine($"El valor de mi Entero es {myInt} y el valor de mi boolean es {myBool}");
+
+            // ----- CONSTANTE -----
+            const string MyConstante = "Mi constante";// en UpperCamelCase !!!
+            Console.WriteLine(MyConstante);
+
+
+
+            //---------------------------- ESTRUCTURAS ----------------------------
+            //---------------------------- ESTRUCTURAS ----------------------------
+            //---------------------------- ESTRUCTURAS ---------------------------- 
+
+            ///****** ARRAY ***** 
+
+            //los arrays (Empiezan en la posicion 0 , 1, 2, 3, 4,etc, hasta la longitud que definamos en la creacion del mismo y dicha longitud no se puede cambiar mas)
+
+            /*
+            //Esto da error!
+
+            var myArray = ["Hora", "Barrios", "Practica"];
+            Console.WriteLine(myArray);
+
+            //Esto da error!
+
+            var myArray = ["Hora", "Barrios", "Practica"];
+            Console.WriteLine(myArray[0]);
+
+            //Esto da error!
+
+            var myArray = new string["Hora", "Barrios", "Practica"];
+            Console.WriteLine(myArray[0]);
+            */
+
+
+            /* asi seria correcto (instanciar el array, agregamos los elementos harcodeados )
+               y conseguir el primer elemento del array.
+            */
+
+            // En C# hay que crearlo con la palabra reservada NEW
+            var myArray = new string[] { "Hora", "Barrios", "Practica" };
+            Console.WriteLine(myArray[0]);
+
+            /*
+            //Esto da error!
+            myArray[3] = "Agregame este string";
+            Console.WriteLine(myArray[3]);
+
+            Cuando creamos un ARRAY tenemos que definirle la longitud y esta es inalterable!
+            por lo tanto ya no podemos agregar mas elementos.
+            */
+
+            myArray[2] = "Practicamos"; // SÍ podemos modificar un elemento existente
+            Console.WriteLine(myArray[2]);
+
+            /*
+            //Esto da error!
+            myArray[2] = 30; // No podemos agregar un tipo de dato distinto al tipo de array creado, ya que en este caso es un arrays de strings
+          
+            Console.WriteLine(myArray[2]);           
+
+            */
+            /*
+             Dato extra:-
+             */
+
+            ///****** DICTIONARY O DICCIONARIOS ***** 
+            var myDictionary = new Dictionary<string, int>
+            {
+                {"Julia", 36 }, // clave en string , valor en entero
+                {"Alberto", 65 },
+                {"Luis", 15 }
+            };
+            Console.WriteLine(myDictionary["Alberto"]);// accedemos al diccionario por su clave aca.
 
 
         }
